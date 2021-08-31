2dc3e57fcb003ee79f9788fa0a6c9db026473d05
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-08-31 00:14:50 -0400

    event delegation

--- js/button.js
+++ js/button.js
@@ -6,10 +6,32 @@ newBtn.innerText = "Click Me!"
 
 divButton.appendChild(newBtn) 
 
-newBtn.addEventListener('click', handleClick) 
+newBtn.addEventListener('click', function(e){
+    console.log(e.target)
+}) 
 
-function handleClick(event){
+const bubble = document.querySelector("#bubble")
 
-    console.log(event)
+bubble.addEventListener('click', function(e){
+    console.log("event bubling clicked", e.target)
+}) 
 
-}
+
+//Event bubbling 
+
+const parentDiv = document.querySelector("#parent") 
+
+parentDiv.addEventListener('click',function(e){
+    //console.log(e.target)
+
+    if(e.target.id === "alert"){
+       alert("ALERT BUTTON CLICKED")
+    }
+    if(e.target.id === "console"){
+       console.log("CONSOLE BUTTON CLICKED")
+    }
+    if(e.target.id === 'error'){
+        console.error("ERROR BUTTON CLICKED")
+    }
+
+})
