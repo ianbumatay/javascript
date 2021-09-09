1f6cec7d32e8689207b379fb2c2e9046d7a83e68
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-09 13:54:40 -0400

    check palindrome using build in function

--- js/palindrome.js
+++ js/palindrome.js
@@ -3,20 +3,48 @@
 
 
 
-function isPalindrome(str){ 
+// function isPalindrome(str){ 
 
-    const strLen = str.length 
+//     const len = str.length 
 
-    for(let i = 0; i <= strLen / 2; i++){ 
+//     for(let i = 0; i <= len / 2; i++){ 
 
-        if(str[i] !== str[strLen - 1 - i]){
-            return false
-        }else{
-            return true
-        }
+//         if(str[i] !== str[len - 1 - i]){ 
+//             return false
+//         }else{
+//             return true
+//         } 
 
+//     }
+
+// }  
+
+function isPalindrome(string){ 
+
+    // Convert string to array 
+    const arrayValues = string.split('') 
+    console.log(arrayValues)
+
+    // Reverse array values 
+    const reverseArrayValues = arrayValues.reverse() 
+    console.log(reverseArrayValues) 
+
+    // Convert array to string 
+    const  reverseString = reverseArrayValues.join('')  
+    console.log(reverseString)
+
+    if(string == reverseString){
+        //console.log("It is a palindrome")
+        return true
+    }else{
+        //console.log("Its not a palindrome")
+        return false
     }
 
-} 
+}
 
-console.log(isPalindrome("kayak"))
+ console.log(isPalindrome("kayak")) 
+
+// string = "This is a string" 
+// str = string.length 
+// console.log(str)
