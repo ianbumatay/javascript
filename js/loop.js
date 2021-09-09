902d603c8da981fdd39c964311891066988386a2
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-09 13:59:48 -0400

    fincd odd and even numbers

--- js/loop.js
+++ js/loop.js
@@ -2,13 +2,22 @@
 
 // for(let i = 1; i <= 10; i++ ){ 
 //     console.log(i)
-// } 
+// }  
+
+
+// print Odd
 
 for(let i = 1; i <= 10; i++){
     if(i % 2 == 1) {
-        console.log(i) 
-    } else if( i % 2 == 0){
-       console.log(i)
+        console.log(i)
     }
     
+} 
+
+//print even 
+
+for(let i = 0; i <= 10; i++){
+    if(i % 2 == 0){
+        console.log("Number " + i  + " is a even number")
+    }
 }
