efe57486fe9c6d0b5c80b391743b603c7f54aa4c
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-23 21:16:30 -0400

    map array method

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -1,14 +1,13 @@
-// Array Methods 
+// Array Methods For Seraching
 // takes in  a callback function funtcion with 4 argument
 // arrayMethod.((element, index, array, thisArg)=> { }) 
 
 let arrOfNums = [100, 2000, 500, 4300, 3500, 2500]
 
 // .find => returns the first element found
-// NO match == undefined
 
 let num = arrOfNums.find((element)=>{ 
-    return element > 6000
+    return element > 6000  // NO match == undefined
 }) 
 console.log(num) //=> returns the first element found 
 
@@ -27,4 +26,13 @@ let findIndexOfNum = arrOfNums.findIndex((e)=> {
 let filterNum = arrOfNums.filter((e)=>{
     return e > 500 //=> return all element that is greater than 500
 }) 
-//console.log(filterNum) //=> [ 2000, 4300, 3500, 2500 ] returns a new array
+//console.log(filterNum) //=> [ 2000, 4300, 3500, 2500 ] returns a new array 
+
+
+// arrayMethod.map()
+
+let names = ["Ian", "Jade", "Jada"] 
+
+names.map((e)=> {
+ console.log(e)
+})
