822f179656e6238ce1098a44002887831845f702
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-27 14:53:02 -0400

    sort method in javascript

--- DSA/Sorting_Algorithm/introduction.js
+++ DSA/Sorting_Algorithm/introduction.js
@@ -22,4 +22,43 @@ function sortStrings(string1 , string2){
 } 
 
 let sortedStrings = ["Ian", "A", "Jade", "Jadot", "Bumatay"].sort(sortStrings)  // sorted each strings by the shortes length in ascending order
-console.log(sortedStrings)
+console.log(sortedStrings) 
+
+//////////////////////
+
+let fruites = ["Orange", "Apple", "Banana", "Grapes"]  
+
+ fruites.sort((a,b)=>{
+     if(a < b){
+        return -1 // change to -1 to  1 to sort decending
+     }
+     if(a > b){
+         return 1 // change 1 to -1 for decending
+     }
+     if(a == b){
+         return 0
+     }
+ })
+
+ //////////////////// 
+
+ let employee = [
+    {id:1, name: "Ian", salary: 10000},
+    {id:1, name: "Jada", salary: 8000},
+    {id:1, name: "Jade", salary: 20000}
+] 
+
+employee.sort((a,b)=> {
+
+   if(a.salary < b.salary){
+       return - 1
+   }
+   if(a.salary > b.salary){
+       return 1
+   }
+   if(a.salary === b.salary){
+       return 0
+   }
+}) 
+
+console.log(employee)
