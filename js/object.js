e2f305575b04b4d3175318b25d129cfef9c840ec
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-27 12:03:35 -0400

    creating obj using constructor functio

--- js/object.js
+++ js/object.js
@@ -1,6 +1,6 @@
 // Creating Obj 
 
-// 1.] Object Literal syntax 
+// ------------- Object Literal syntax 
 
 var dog = {
     // object properties
@@ -18,17 +18,39 @@ var dog = {
     }
 } 
 
-// 2.] New keyword  => 
+// ---------------  Using new keyword  => Object.new
 
-const student = Object.new 
+// const student = Object.new 
 
-//creating properties 
-student.name = "Ian" 
-student.lastName = "Bumatay"
-student.gpa = 3
-student.subjects = ["Math", "Programming", "Algorithm"]
+// //setting up properties 
+// student.name = "Ian" 
+// student.lastName = "Bumatay"
+// student.gpa = 3
+// student.subjects = ["Math", "Programming", "Algorithm"]
 
-// methods 
-student.getSubjects = function(){
-    return this.subjects
-}
+// // methods 
+// student.getSubjects = function(){
+//     return this.subjects
+// } 
+
+// -------------- Using a constructor function 
+
+
+function Car(brand, color, year, engine){  // note: Carc onstructor function starts with capital letter   
+// setting up properties
+    this.brand = brand 
+    this.color = color 
+    this.year = year 
+    this.engine = engine
+} 
+
+// creating methods
+Car.prototype.getColor = function(){ // 
+    return this.color
+} 
+
+// creating new object 
+
+const Rav4 = new Car("Toyota", "Black", "2017", "v4") 
+
+console.log(Rav4)
