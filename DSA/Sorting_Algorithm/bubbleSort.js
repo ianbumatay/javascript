ff613a4f84577c1d72cfa72534acc68d1b9c3228
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-28 14:08:50 -0400

    Optimize bubble sort

--- DSA/Sorting_Algorithm/bubbleSort.js
+++ DSA/Sorting_Algorithm/bubbleSort.js
@@ -15,22 +15,43 @@
 //     nextElement = temp
 // } 
 
-function bubbleSort(arr){
+// function bubbleSort(arr){
 
-    for(var i = 0; i < arr.length; i++){     // loop every element in the array starting index 0 (i = 0) to the last element in the array (arr.lenth)
+//     for(var i = 0; i < arr.length; i++){     // loop every element in the array starting index 0 (i = 0) to the last element in the array (arr.lenth)
 
-        for(var j = 0; j < arr.length; j++){   // loop every element in array that is not sorted
+//         for(var j = 0; j < arr.length; j++){   // loop every element in array that is not sorted
 
-            if(arr[j] > arr[j + 1]){        // if current element (arr[j])  is greater than next element (arr[j + 1])
-                 // swap                    // swap elements 
-                 let temp = arr[j]
-                 arr[j] = arr[j+1]
-                 arr[j+1] = temp
+//             if(arr[j] > arr[j + 1]){        // if current element (arr[j])  is greater than next element (arr[j + 1])
+//                  // swap                    // swap elements 
+//                  let temp = arr[j]
+//                  arr[j] = arr[j+1]
+//                  arr[j+1] = temp
+//             }
+//         }
+//     }
+//     return arr
+// } 
+
+// let result = bubbleSort([37, 45, 39, 8])
+// console.log(result) //=> [ 8, 37, 39, 45 ] 
+
+// Bubble sort Optimization 
+
+function OptimizeBubbleSort(arr){ 
+
+    for(var i = arr.length; i > 0; i--){  // arr.length = 4
+
+        for(var j = 0; j < i - 1; j++){ 
+
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
             }
         }
     }
-    return arr
+    return arr 
 } 
 
-let result = bubbleSort([37, 45, 39, 8])
-console.log(result)
+let result = OptimizeBubbleSort([37, 45, 39, 8])
+console.log(result) //=> [ 8, 37, 39, 45 ] 
