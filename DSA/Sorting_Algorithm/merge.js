59169949cad7b4bf75f64b9453900b946021a9b2
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-30 16:41:20 -0400

    implemented merge sorting algorithm

--- DSA/Sorting_Algorithm/merge.js
+++ DSA/Sorting_Algorithm/merge.js
@@ -1,34 +1,6 @@
 // Merge Sort Algorithm 
 
-//merge function   
-
-//  Merges two already sorted arrays
-// function merge(arr1, arr2){
-//     let results = [];
-//     let i = 0;
-//     let j = 0;
-//     while(i < arr1.length && j < arr2.length){
-//         if(arr2[j] > arr1[i]){
-//             results.push(arr1[i]);
-//             i++;
-//         } else {
-//             results.push(arr2[j])
-//             j++;
-//         }
-//     }
-//     while(i < arr1.length) {
-//         results.push(arr1[i])
-//         i++;
-//     }
-//     while(j < arr2.length) {
-//         results.push(arr2[j])
-//         j++;
-//     }
-//     return results;
-// }
-// merge([100,200], [1,2,3,5,6])
-
-
+// merge function   
 function merge(arr1, arr2){ 
     let results = [] // new array to house the sorted elements
     let startIndexOfArr1 = 0  // starting index of arr1
@@ -57,5 +29,18 @@ function merge(arr1, arr2){
 
 } 
 
-result = merge([100,200], [1,2,3,5,6])
+// result = merge([100,200], [1,2,3,5,6])
+// console.log(result) 
+
+// Recrusive Merge Sort
+
+function mergeSort(arr){
+    if(arr.length <= 1) return arr
+    let mid = Math.floor(arr.length/2)       // split the array
+    let left = mergeSort(arr.slice(0, mid)) // left side of the array
+    let right = mergeSort(arr.slice(mid))   // right side of the array
+    return merge(left, right)
+} 
+
+result = mergeSort([10,1,76,73]) 
 console.log(result)
